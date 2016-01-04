38d5ad117e21bfef36fcfaa0e82245e4e0367562
Jordan Harband <ljharb@gmail.com>
2016-01-03 17:12:29 -0800

    [Fix: ES5] Remove incorrect bypassing of [[DefaultValue]] implementation.
    Fixes #2.

--- es5.js
+++ es5.js
@@ -9,7 +9,7 @@ var isCallable = require('is-callable');
 // https://es5.github.io/#x8.12
 var ES5internalSlots = {
 	'[[DefaultValue]]': function (O, hint) {
-		var actualHint = hint || toStr.call(O) === '[object Date]' ? String : Number;
+		var actualHint = hint || (toStr.call(O) === '[object Date]' ? String : Number);
 
 		if (actualHint === String || actualHint === Number) {
 			var methods = actualHint === String ? ['toString', 'valueOf'] : ['valueOf', 'toString'];
@@ -33,16 +33,5 @@ module.exports = function ToPrimitive(input, PreferredType) {
 	if (isPrimitive(input)) {
 		return input;
 	}
-	var preferredType = PreferredType;
-	if (arguments.length < 2) {
-		preferredType = toStr.call(input) === '[object Date]' ? String : Number;
-	}
-	if (preferredType === String) {
-		return String(input);
-	} else if (preferredType === Number) {
-		return Number(input);
-	} else {
-		throw new TypeError('invalid PreferredType supplied');
-	}
-	return ES5internalSlots['[[DefaultValue]]'](input, preferredType);
+	return ES5internalSlots['[[DefaultValue]]'](input, PreferredType);
 };
