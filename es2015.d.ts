c22114883f40be2cb67bb7099162478642021fbd
Jordan Harband <ljharb@gmail.com>
2026-06-25 12:24:19 -0700

    [types] add ES6 (non-bigint) primitive types

--- es2015.d.ts
+++ es2015.d.ts
@@ -3,6 +3,12 @@ import type { primitive } from './';
 declare function ToPrimitive(
     input: unknown,
     hint?: StringConstructor | NumberConstructor,
-): primitive;
+): primitive | ToPrimitive.primitiveES6;
+
+
+declare namespace ToPrimitive {
+    export type primitiveES6 = Exclude<primitive, bigint>;
+    export type unknownES6 = primitiveES6 | object;
+}
 
 export = ToPrimitive;
