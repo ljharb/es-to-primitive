73d7fa3a67241152b5602f45e5f14fc5c3d72fdf
Jordan Harband <ljharb@gmail.com>
2026-06-25 18:38:11 -0700

    [Robustness] use `es-define-property`

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var defineProperty = require('es-define-property');
+
 var ES5 = require('./es5');
 var ES6 = require('./es6');
 var ES2015 = require('./es2015');
@@ -11,9 +13,9 @@ module.exports.ES5 = ES5;
 module.exports.ES6 = ES6;
 module.exports.ES2015 = ES2015;
 
-if (Object.defineProperty) {
+if (defineProperty) {
 	// `ES2015` is `module.exports`; defining here rather than on `module.exports` keeps the lexer from de-listing the above
-	Object.defineProperty(ES2015, 'ES5', { enumerable: false });
-	Object.defineProperty(ES2015, 'ES6', { enumerable: false });
-	Object.defineProperty(ES2015, 'ES2015', { enumerable: false });
+	defineProperty(ES2015, 'ES5', { enumerable: false });
+	defineProperty(ES2015, 'ES6', { enumerable: false });
+	defineProperty(ES2015, 'ES2015', { enumerable: false });
 }
